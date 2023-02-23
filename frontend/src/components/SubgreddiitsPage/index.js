f5bf200ffbcf9e529e6f781519162b5e1dd70fcc
Prakul <prakulagrawal.2003@gmail.com>
2023-02-24 04:39:25 +0530

    Implemented sorting of subgreddiits

--- frontend/src/components/SubgreddiitsPage/index.js
+++ frontend/src/components/SubgreddiitsPage/index.js
@@ -10,6 +10,8 @@ import axios from "axios";
 function SubgreddiitsPage() {
   const [user, setUser] = useRecoilState(userState);
   const [notJoined, setNotJoined] = useState([]);
+  const [sorted, setSorted] = useState([]);
+  const [isSorted, setIsSorted] = useState(false);
 
   const getNotJoined = async () => {
     const config = {
@@ -19,9 +21,26 @@ function SubgreddiitsPage() {
     };
     try {
       const response = await axios.get("/api/subgreddiit/notjoined", config);
-      console.log(response.data);
       setNotJoined(response.data);
-      console.log(notJoined);
+    } catch (err) {
+      if (err.response) {
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+        alert(err.response.data.msg);
+      } else if (err.request) {
+        console.log(err.request);
+      } else {
+        console.log(err.message);
+      }
+    }
+  };
+
+  const getSorted = async (sort_type) => {
+    try {
+      const response = await axios.get(`/api/subgreddiit/sort/${sort_type}`);
+      setSorted(response.data);
+      setIsSorted(true);
     } catch (err) {
       if (err.response) {
         console.log(err.response.data);
@@ -37,7 +56,6 @@ function SubgreddiitsPage() {
   };
 
   useEffect(() => {
-    console.log(user);
     getNotJoined();
   }, []);
 
@@ -190,15 +208,116 @@ function SubgreddiitsPage() {
     </div>
   ));
 
+  const sortedSubgreddiits = sorted.map((s) => (
+    <div key={s._id} className="mb-3">
+      <Card sx={{ maxWidth: "50vw", margin: "auto" }}>
+        <CardContent>
+          <div className="text-center text-2xl">{s.name}</div>
+          <div className="text-center mb-3">{s.description}</div>
+          <div className="flex mb-3">
+            <div className="w-1/2 text-center">
+              Number of Followers: {s.followers_count}
+            </div>
+            <div className="w-1/2 text-center">
+              Number of Posts: {s.posts_count}
+            </div>
+          </div>
+          <div className="text-center mb-2">
+            Tags: {s.tags.map((x) => x + " ")}
+          </div>
+          <div className="text-center">
+            Banned Words: {s.banned.map((x) => x + " ")}
+          </div>
+        </CardContent>
+        <CardActions>
+          {s.moderator === user._id ? (
+            <Button size="small" disabled>
+              Leave Subgreddiit
+            </Button>
+          ) : s.followers.includes(user._id) ? (
+            <Button
+              size="small"
+              onClick={() => {
+                leave(s._id);
+              }}
+            >
+              Leave Subgreddiit
+            </Button>
+          ) : (
+            <Button
+              size="small"
+              onClick={() => {
+                sendRequest(s._id);
+              }}
+            >
+              Send Join Request
+            </Button>
+          )}
+        </CardActions>
+      </Card>
+    </div>
+  ));
+
   return (
     <>
       <div className="flex mx-auto text-7xl font-extrabold text-white m-5">
         Subgreddits Page
       </div>
-      <div className="flex flex-col">
-        {joinedSubgreddiits}
-        {notJoinedSubgreddiits}
+      <div className="flex justify-center mb-7">
+        <Button
+          variant="contained"
+          sx={{ mx: 5 }}
+          onClick={() => {
+            getSorted("name_asc");
+          }}
+        >
+          Sort by Name (Asc)
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ mx: 5 }}
+          onClick={() => {
+            getSorted("name_desc");
+          }}
+        >
+          Sort by Name (Desc)
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ mx: 5 }}
+          onClick={() => {
+            getSorted("follow");
+          }}
+        >
+          Sort by Followers
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ mx: 5 }}
+          onClick={() => {
+            getSorted("creation");
+          }}
+        >
+          Sort by Creation Date
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ mx: 5 }}
+          onClick={() => {
+            setIsSorted(false);
+          }}
+        >
+          Remove Sort
+        </Button>
       </div>
+      {isSorted ? (
+        <div className="flex flex-col">{sortedSubgreddiits}</div>
+      ) : (
+        <div className="flex flex-col">
+          {joinedSubgreddiits}
+          {notJoinedSubgreddiits}
+        </div>
+      )}
     </>
   );
 }
